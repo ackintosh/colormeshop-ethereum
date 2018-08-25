be5c54f9e8a8cac9b4921f1c186cbba7dcab3898
akihito.nakano <sora.akatsuki@gmail.com>
2018-08-25 20:34:53 +0900

    Add ethereum client

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'ethereum'
 
 get '/' do
+  client = Ethereum::HttpClient.new('http://localhost:7545')
+  contract = Ethereum::Contract.create(name: 'Colormeshop', truffle: { paths: ['./']}, client: client)
+  p contract.transact_and_wait.add_product_transaction('test_product_code', 3)
+  p contract.call.product_transactions()
   'ColorMeShop x Ethereum!'
 end
