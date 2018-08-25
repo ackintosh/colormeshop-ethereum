b81f2501ba94597516b8ec9bb6bba8f3a27ac76e
akihito.nakano <sora.akatsuki@gmail.com>
2018-08-25 21:38:04 +0900

    Add ColorMeShop api client

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'ethereum'
+require 'color_me_shop'
 
 get '/' do
+  sale_api = sale_api(ENV['COLORMESHOP_ACCESS_TOKEN'])
+  p sale_api.get_sales
+
   client = Ethereum::HttpClient.new('http://localhost:7545')
   contract = Ethereum::Contract.create(name: 'Colormeshop', truffle: { paths: ['./']}, client: client)
   count = contract.call.product_transaction_count
@@ -12,4 +16,12 @@ get '/' do
     rows << "<div>#{product_transaction[0]}: #{product_transaction[1]}</div>"
   end
   "<html><head><title>ColorMeShop x Ethereum</title></head><body><h1>ColorMeShop x Ethereum!</h1><h2>transactions</h2>#{rows.join}</body></html>"
+end
+
+def sale_api(token)
+  ColorMeShop.configure do |config|
+    config.access_token = token
+  end
+
+  ColorMeShop::SaleApi.new
 end
