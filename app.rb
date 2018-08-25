5e2ba77828b971e2e19e7026e752acafdf5d5080
akihito.nakano <sora.akatsuki@gmail.com>
2018-08-25 21:27:18 +0900

    Returns transactions obtained from ethereum as html

--- app.rb
+++ app.rb
@@ -5,7 +5,11 @@ require 'ethereum'
 get '/' do
   client = Ethereum::HttpClient.new('http://localhost:7545')
   contract = Ethereum::Contract.create(name: 'Colormeshop', truffle: { paths: ['./']}, client: client)
-  p contract.transact_and_wait.add_product_transaction('test_product_code', 3)
-  p contract.call.product_transactions()
-  'ColorMeShop x Ethereum!'
+  count = contract.call.product_transaction_count
+  rows = []
+  count.times do |i|
+    product_transaction = contract.call.product_transactions(i)
+    rows << "<div>#{product_transaction[0]}: #{product_transaction[1]}</div>"
+  end
+  "<html><head><title>ColorMeShop x Ethereum</title></head><body><h1>ColorMeShop x Ethereum!</h1><h2>transactions</h2>#{rows.join}</body></html>"
 end
